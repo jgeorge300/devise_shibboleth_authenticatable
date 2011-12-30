12082603f76661a39936f6808507fd4b04d266c8
Joe George <joe.george@osumc.edu>
2011-12-30 10:52:43 -0500

    For optional Shibb authentication, account creation needs to be in the session controller

--- app/controllers/devise/shibboleth_sessions_controller.rb
+++ app/controllers/devise/shibboleth_sessions_controller.rb
@@ -2,6 +2,10 @@ class Devise::ShibbolethSessionsController < Devise::SessionsController
   unloadable
   
   def new
+    if request.env['eppn']
+      authenticate_with_shibboleth request.env
+    end
+
     resource = build_resource
     shib_config = YAML.load(ERB.new(File.read(::Devise.shibboleth_config || "#{Rails.root}/config/shibboleth.yml")).result)[Rails.env]
 
@@ -17,5 +21,40 @@ class Devise::ShibbolethSessionsController < Devise::SessionsController
 
   def destroy
     # Can't fully log out from Shibboleth until the browser is closed.
+    super
   end
+
+  private
+  # Repeating Myself.  Need to move to a helper
+  def authenticate_with_shibboleth(env)
+
+    resource = User.find_by_email(env['eppn'])
+
+    if (!resource.nil? && !Devise.shibboleth_create_user)
+      logger.warn("User(#{env['eppn']}) not found.  Not configured to create the user.")
+      return resource
+    end
+
+    if (resource.nil? && Devise.shibboleth_create_user)
+      logger.fatal("Creating user(#{env['eppn']}).")
+      resource = User.new()
+    end
+    return nil unless resource
+
+    save_user_shibboleth_headers(resource, env)
+
+    resource.save
+    resource
+  end
+
+  def save_user_shibboleth_headers(user, env)
+    shib_config = YAML.load(ERB.new(File.read(::Devise.shibboleth_config || "#{Rails.root}/config/shibboleth.yml")).result)[Rails.env]
+    shib_config['user-mapping'].each do |model, header|
+      logger.info("Saving #{env[header]} to #{model}")
+      field = "#{model}="
+      value = env[header]
+      user.send(field, value.to_s) 
+    end
+  end
+
 end
